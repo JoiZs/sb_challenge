3b2eab1dac7e480aabca4257c7e7dec73cff8431
JoiZs <pyaesoneaung1126@hotmail.com>
2024-11-12 09:38:27 -0500

    feat(init apollo server): setup schema, & standalone server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,21 @@
 import "dotenv/config";
 import "reflect-metadata";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from "@apollo/server";
+import { apolloSchema } from "./utils/apolloSchema";
+import { PrismaClient } from "@prisma/client";
+import { ApCtx } from "./context/apolloctx";
+
+(async () => {
+  const prisma = new PrismaClient();
+  const server = new ApolloServer<ApCtx>({
+    schema: await apolloSchema,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: parseInt(process.env.PORT!) },
+    context: async () => ({ prisma }),
+  });
+
+  console.log("Server UP! ", url);
+})();
