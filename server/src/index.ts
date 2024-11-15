36f1a740c572e0a0790d8a4e7594622ff2a04424
JoiZs <pyaesoneaung@hotmail.com>
2024-11-15 08:27:49 -0500

    (fix) standalone server to apolloexpress

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,41 @@
 import "dotenv/config";
 import "reflect-metadata";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
+import http from "http";
+import cors from "cors";
+import express from "express";
 import { apolloSchema } from "./utils/apolloSchema";
 import { PrismaClient } from "@prisma/client";
 import { ApCtx } from "./context/apolloctx";
 
 (async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
   const prisma = new PrismaClient();
   const server = new ApolloServer<ApCtx>({
     schema: await apolloSchema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: parseInt(process.env.PORT!) },
-    context: async () => ({ prisma }),
-  });
+  await server.start();
 
-  console.log("Server UP! ", url);
+  app.use(
+    "/",
+    cors<cors.CorsRequest>({
+      origin:
+        process.env.NODE_ENV == "production"
+          ? ["pyaesoneaung.com", "localhost"]
+          : ["localhost"],
+    }),
+    express.json({ limit: "50mb" }),
+    expressMiddleware(server, {
+      context: async () => ({ prisma }),
+    })
+  );
+
+  httpServer.listen({ port: process.env.PORT });
+
+  console.log("Server UP! ", `http://localhost:${process.env.PORT}/`);
 })();
